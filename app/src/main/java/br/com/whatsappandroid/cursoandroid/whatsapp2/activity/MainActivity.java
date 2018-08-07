12847b81253561320ee61dd25d5a5ad973733079
daniloamsilva <danilo_dams@yahoo.com.br>
2018-08-07 19:50:37 -0300

    Aula 191 - Login de Usuário

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package br.com.whatsappandroid.cursoandroid.whatsapp2.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -11,10 +15,25 @@ import br.com.whatsappandroid.cursoandroid.whatsapp2.config.ConfiguracaoFirebase
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button botaoSair;
+    private FirebaseAuth autenticacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoSair = findViewById(R.id.bt_sair);
+
+        botaoSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
