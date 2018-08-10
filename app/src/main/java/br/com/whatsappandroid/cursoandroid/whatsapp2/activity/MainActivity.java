2905d2b36842100a11ce7c5d0a4d584439f8eb0c
daniloamsilva <danilo_dams@yahoo.com.br>
2018-08-09 22:02:22 -0300

    Aula 196 - Criando uma ToolBar

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/activity/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.whatsappandroid.cursoandroid.whatsapp2.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,25 +18,25 @@ import br.com.whatsappandroid.cursoandroid.whatsapp2.config.ConfiguracaoFirebase
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button botaoSair;
     private FirebaseAuth autenticacao;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoSair = findViewById(R.id.bt_sair);
-
-        botaoSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
-                autenticacao.signOut();
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
