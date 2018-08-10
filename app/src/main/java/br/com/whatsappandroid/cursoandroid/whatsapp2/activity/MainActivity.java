e136e22bd1f89fa09ad348b7feb4f7d116892853
daniloamsilva <danilo_dams@yahoo.com.br>
2018-08-09 22:36:04 -0300

    Aula 198 - Deslogar usuário & Configurações da Toolbar

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
@@ -37,6 +40,27 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch ( item.getItemId() ){
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.item_config:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deslogarUsuario(){
+        autenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
