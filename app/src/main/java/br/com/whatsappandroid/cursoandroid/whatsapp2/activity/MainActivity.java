af861fb597c362882b84923f9f49e46b8b3c092c
daniloamsilva <danilo_dams@yahoo.com.br>
2018-08-13 20:13:20 -0300

    Aula 213 - Adicionando Contatos - Parte 1

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/activity/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.whatsappandroid.cursoandroid.whatsapp2.activity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
@@ -14,14 +15,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import br.com.whatsappandroid.cursoandroid.whatsapp2.R;
 import br.com.whatsappandroid.cursoandroid.whatsapp2.adapter.TabAdapter;
 import br.com.whatsappandroid.cursoandroid.whatsapp2.config.ConfiguracaoFirebase;
+import br.com.whatsappandroid.cursoandroid.whatsapp2.helper.Base64Custom;
 import br.com.whatsappandroid.cursoandroid.whatsapp2.helper.SlidingTabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
 
     private SlidingTabLayout slidingTabLayout;
     private ViewPager viewPager;
+
+    private String identificadorContato;
+    private DatabaseReference firebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,12 +98,44 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.setMessage("E-mail do usuário");
         alertDialog.setCancelable(false);
 
-        EditText editText = new EditText(MainActivity.this);
+        final EditText editText = new EditText(MainActivity.this);
         alertDialog.setView( editText );
 
         alertDialog.setPositiveButton("Adicionar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+
+                String emailContato = editText.getText().toString();
+
+                //Valida se o e-mail foi digitado
+                if( emailContato.isEmpty() ){
+                    Toast.makeText(MainActivity.this, "Preencha o e-mail", Toast.LENGTH_LONG).show();
+                }else{
+                    //Verificar se o usuário já está cadastrado no nosso App
+                    identificadorContato = Base64Custom.codificarBase64(emailContato);
+
+                    //Recuperar instância Firebase
+                    firebase = ConfiguracaoFirebase.getFirebase().child("usuarios").child(identificadorContato);
+
+                    firebase.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if( dataSnapshot.getValue() != null ){
+
+                                firebase = ConfiguracaoFirebase.getFirebase();
+                                firebase = firebase.child("contatos")
+                                        .child("");
+                            }else{
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
 
             }
         });
--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/helper/Preferencias.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/helper/Preferencias.java
@@ -11,10 +11,17 @@ public class Preferencias {
     private final int MODE = 0;
     private SharedPreferences.Editor editor;
 
+    private final String CHAVE_IDENTIFICADOR = "identificadorUsuarioLogado";
+
     public Preferencias( Context contextoParametro) {
         contexto = contextoParametro;
         preferences = contexto.getSharedPreferences(NOME_ARQUIVO, MODE );
         editor = preferences.edit();
+    }
+
+    public void salvarDados( String identificadorUsuario ){
+        editor.putString(CHAVE_IDENTIFICADOR, identificadorUsuario);
+        editor.commit();
     }
 
 }
