ca9ed97f9edbf16e9c361a831aace4141edbd593
daniloamsilva <danilo_dams@yahoo.com.br>
2018-08-15 21:41:12 -0300

    Aula 216 - Listando Contatos - Parte 1

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/fragment/ContatosFragment.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp2/fragment/ContatosFragment.java
@@ -7,14 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import br.com.whatsappandroid.cursoandroid.whatsapp2.R;
+import br.com.whatsappandroid.cursoandroid.whatsapp2.model.Contato;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
 
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private ArrayList<String> contatos;
 
     public ContatosFragment() {
         // Required empty public constructor
@@ -24,8 +32,25 @@ public class ContatosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Instânciar objetos
+        contatos = new ArrayList<>();
+        contatos.add("Mariana Silva");
+        contatos.add("Leticia Almeida");
+        contatos.add("José Renato");
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        listView = view.findViewById(R.id.lv_contatos);
+        adapter = new ArrayAdapter(
+                getActivity(),
+                R.layout.lista_contato,
+                contatos
+        );
+        listView.setAdapter(adapter);
+
+        return view;
     }
 
 }
